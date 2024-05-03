620d64b78a98a4f6df4446d7d7bacc6db3f42d3a
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2024-05-03 20:23:52 +0200

    added IDamageableScript

--- Operation Oil Airstrike/Assets/Scripts/Planes/Health/PlaneHealth.cs
+++ Operation Oil Airstrike/Assets/Scripts/Planes/Health/PlaneHealth.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PlaneHealth : MonoBehaviour
+public class PlaneHealth : MonoBehaviour, IDamageable
 {
     private PlaneStats _planeStats;
     void Start()
@@ -8,5 +8,11 @@ public class PlaneHealth : MonoBehaviour
         _planeStats = GetComponent<PlaneStats>();
     }
 
-    
+    public void TakeDamage(float damage)
+    {
+        _planeStats.health -= damage;
+        Debug.Log(_planeStats.health + "Health Updated");
+    }
 }
+
+
