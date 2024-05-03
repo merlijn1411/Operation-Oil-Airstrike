faeafd4f33a0baafcd758d638a86c4385a605f5c
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2024-05-03 20:09:16 +0200

    renamed prefab to bullet and add somethin on bullet script

--- Operation Oil Airstrike/Assets/Scripts/Planes/Guns/Bullet.cs
+++ Operation Oil Airstrike/Assets/Scripts/Planes/Guns/Bullet.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private float Damage;
+    [SerializeField] private float damage;
+    [SerializeField] private float timer;
     private void Awake()
     {
-        Destroy(gameObject, 1.5f);
+        Destroy(gameObject, timer);
     }
+    
 }
